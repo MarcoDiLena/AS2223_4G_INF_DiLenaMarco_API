58900a09c577801249b97ebd6684c5bbcfc65339
PC <PC@INFO5-5097>
2023-03-16 08:14:49 +0100

    Aggiungere i file di progetto.

---
+++ AS2223_4G_INF_DiLenaMarco_API/Controllers/MathController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AS2223_4G_INF_DiLenaMarco_API.Controllers
+{
+    public class MathController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
